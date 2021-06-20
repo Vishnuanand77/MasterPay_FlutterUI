55e2cbdee729a66a4ee611d79a489fcd459f3185
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-20 22:41:46 +0530

    Initialized Project

---
+++ android/app/src/main/java/com/vishnu/anand/expenses_app_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.vishnu.anand.expenses_app_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
