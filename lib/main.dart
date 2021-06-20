1bc842b1856029c13da128fd678e88c680c5d77a
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-20 22:55:18 +0530

    Created a header class file

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
+import 'package:expenses_app_ui/widgets/walletheader.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomePage(),
+    theme: ThemeData(fontFamily: 'Roboto'),
   ));
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Column(
+        children: [
+          Container(
+            height: 120,
+            child: WalletHeader(),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+        ],
+      ),
     );
   }
 }
