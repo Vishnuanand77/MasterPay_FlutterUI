fe7f7a6c47268ff36742384db9395624bb33af0b
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-20 23:01:58 +0530

    Fixed Dependencies

---
+++ lib/data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color primaryColor = Color(0xFFCADCED);
+
+List<BoxShadow> customShadow = [
+  BoxShadow(
+    color: Colors.white.withOpacity(0.5),
+  )
+];
