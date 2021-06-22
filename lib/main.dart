dbc479cf0fbe2a0de4f8b62c06d1750d6352d0c9
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-22 13:24:39 +0530

    Made Android status bar transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
 import 'package:expenses_app_ui/data.dart';
 import 'package:expenses_app_ui/widgets/walletheader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'widgets/ExpensesSection.dart';
 import 'widgets/cardSection.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
   runApp(MaterialApp(
     home: HomePage(),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(fontFamily: 'Roboto'),
+    
   ));
 }
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+  
 
   @override
   Widget build(BuildContext context) {
