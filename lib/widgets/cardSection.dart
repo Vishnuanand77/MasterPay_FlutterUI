1e63c98c111073189fc2d207ac07a7e8694933a1
Vishnu Anand <vishnuanand2000@gmail.com>
2021-06-22 11:53:06 +0530

    Created mastercard UI

--- lib/widgets/cardSection.dart
+++ lib/widgets/cardSection.dart
@@ -1,12 +1,71 @@
+import 'package:expenses_app_ui/data.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CardSection extends StatelessWidget {
-  const CardSection({ Key? key }) : super(key: key);
+  const CardSection({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Card Selected',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+        Expanded(
+          child: ListView.builder(
+              itemCount: 2,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (ctx, i) {
+                return Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 40,
+                  ),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: primaryColor,
+                    boxShadow: customShadow,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        top: 150,
+                        bottom: -200,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white38,
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        left: -300,
+                        top: -100,
+                        bottom: -100,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            boxShadow: customShadow,
+                            shape: BoxShape.circle,
+                            color: Colors.white38,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ],
     );
   }
 }
